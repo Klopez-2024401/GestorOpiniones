fb3e75ef367a27a08f5071d8e27e34e8d4149745
klopez-2024401 <klopez-2024401@kinal.edu.gt>
2026-02-16 23:17:28 -0600

    Configuración final del servidor y registro de rutas en index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+
+import { connectDB } from './configs/db.js';
+
+import authRoutes from './src/User/auth.routes.js';
+import postRoutes from './src/Post/post.routes.js';
+import commentRoutes from './src/Comment/comment.routes.js';
+
+dotenv.config();
+
+const app = express();
+connectDB();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
+app.use(limiter);
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
